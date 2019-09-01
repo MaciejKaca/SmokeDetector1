c1e399d465df4338a66b864be8c07cac1288f85b
MaciejKaca <maciek.kaca@gmail.com>
2019-09-01 13:38:08 +0200

    Add project files.

---
+++ SmokeDetector/src/SmokeDetector.cpp
@@ -0,0 +1,10 @@
+/*
+ Name:		SmokeDetector.cpp
+ Created:	9/1/2019 1:12:41 PM
+ Author:	Maciej Kaca
+ Editor:	http://www.visualmicro.com
+*/
+
+#include "SmokeDetector.h"
+
+
---
+++ SmokeDetector/src/SmokeDetector.h
@@ -0,0 +1,19 @@
+/*
+ Name:		SmokeDetector.h
+ Created:	9/1/2019 1:12:41 PM
+ Author:	Maciej Kaca
+ Editor:	http://www.visualmicro.com
+*/
+
+#ifndef _SmokeDetector_h
+#define _SmokeDetector_h
+
+#if defined(ARDUINO) && ARDUINO >= 100
+	#include "arduino.h"
+#else
+	#include "WProgram.h"
+#endif
+
+
+#endif
+
